535e0a79c34f24818aea28d290597371bc5f5979
tka demo <tkademo1@gmail.com>
2023-11-20 12:28:15 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from model import predict_price
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    prediction = None
+    if request.method == 'POST':
+        try:
+            feature_value = float(request.form['feature_value'])
+            prediction = predict_price(feature_value)
+        except ValueError:
+            prediction = "Invalid input. Please enter a valid number."
+
+    return render_template('index.html', prediction=prediction)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
---
+++ model.py
@@ -0,0 +1,7 @@
+# Simple linear regression model for demonstration
+# This example assumes a model y = 2x + 1
+
+def predict_price(feature_value):
+    # In a real-world scenario, you would load a trained model
+    # and use it to predict based on the input feature_value
+    return 2 * feature_value + 1
