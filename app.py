f2a0e8a72ce73a1726c6a2fa6d04e15a7818d385
tka demo <tkademo1@gmail.com>
2023-11-20 13:39:54 +0530

    fincl commit

--- app.py
+++ app.py
@@ -16,5 +16,5 @@ def index():
     return render_template('index.html', prediction=prediction)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=5000,debug=True)
 
